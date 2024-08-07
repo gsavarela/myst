31ed71b1e4b9303542072cb7e10bb6b555b56537
Guilherme Varela <guilhermevarela@protonmail.com>
2024-08-07 18:43:50 +0100

    Change font to JetBrainsMonoNerdFont for NvChad

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrainsMono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "JetBrainsMonoNerdFont:pixelsize=12:antialias=true:autohint=true";
 
 /* Spare fonts */
 static char *font2[] = {
